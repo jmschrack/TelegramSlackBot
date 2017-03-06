7bfe39cd6d7b58751dfcff6e02cbb1477771b684
Jonathan Schrack <jmschrack@gmail.com>
2017-03-05 23:34:34 -0600

    swapped file creation to a more logical and compatible way

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
+#install the node packages
 npm install node-telegram-bot-api slackbots slack-node scrape-it jsonfile -save
+#set permissions
 chmod a+x run.sh
+#install daemontools
 apt-get -y install daemontools-run daemontools
+#make daemontools service folder for bot
 mkdir /etc/service/tsbot
-echo -e "#!/bin/bash \r" >> /etc/service/tsbot/run
-echo `pwd` >> /etc/service/tsbot/run
-echo "/run.sh" >> /etc/service/tsbot/run
+#create the run file
+cat <<EOT > /etc/service/tsbot/run
+#!/bin/bash
+`pwd`/run.sh
+EOT
 chmod +x /etc/service/tsbot/run
